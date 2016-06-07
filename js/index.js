df2b3e2457028a9dde1718edfb428fb3431347e7
Fahad Hossain <oneloopone@gmail.com>
2016-06-07 19:45:00 +0600

    append rather than replace

--- js/index.js
+++ js/index.js
@@ -3,12 +3,12 @@
   const siteUrl = "http://fa7ad.github.io/imgcdn/";
   let list = "";
 
-  $.get("https://api.github.com/repos/fa7ad/imgcdn/contents").done(files => {
-    files.forEach(file => {
+  $.getJSON("https://api.github.com/repos/fa7ad/imgcdn/contents").done(json => {
+    json.forEach(file => {
       if (/(jpeg|jpg|gif|bmp|ico|png|svg)$/.test(file.path))
         list += `<li><a href="${siteUrl + file.path}">${file.name}</a></li>`;
     });
-    $("#list").html(list);
+    $("#list").append(list);
   });
 
 })(jQuery);
