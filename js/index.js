f6601dcf02bc40c057bb8075e9033fa5f8835395
Fahad Hossain <oneloopone@gmail.com>
2016-06-07 19:32:12 +0600

    use promises -- for no reason at all

--- js/index.js
+++ js/index.js
@@ -1,18 +1,14 @@
 ($ => {
   "use strict";
   const siteUrl = "http://fa7ad.github.io/imgcdn/";
-  let listElements = "";
+  let list = "";
 
-  $.ajax({
-    url: "https://api.github.com/repos/fa7ad/imgcdn/contents",
-    method: "get",
-    success: files => {
-      files.forEach(file => {
-        if (/(jpeg|jpg|gif|bmp|ico|png|svg)$/.test(file.path))
-          listElements += `<li><a href="${siteUrl + file.path}">${file.name}</a></li>`;
-      });
-      $("#list").html(listElements);
-    }
+  $.get("https://api.github.com/repos/fa7ad/imgcdn/contents").done(files => {
+    files.forEach(file => {
+      if (/(jpeg|jpg|gif|bmp|ico|png|svg)$/.test(file.path))
+        list += `<li><a href="${siteUrl + file.path}">${file.name}</a></li>`;
+    });
+    $("#list").html(list);
   });
 
 })(jQuery);
